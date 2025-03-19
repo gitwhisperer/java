b1b00651cb960c1c267ab2f499fbd16e805b5a75
GitWhisperer <harshittinker1979@gmail.com>
2025-03-19 15:22:49 +0530

    added files

---
+++ RelationalOperators.java
@@ -0,0 +1,15 @@
+//wajp to demonstrate relational opertors in java
+class Main RelationalOperators
+{
+    public static void main(String[] args) 
+    {
+        int a=10;
+        int b=20;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+    }
+}
