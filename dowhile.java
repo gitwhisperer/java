efd5c244ad7b59e244abdcf32978a1d4f49ab215
GitWhisperer <harshittinker1979@gmail.com>
2025-03-19 16:52:07 +0530

    added files again third time

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+// Date created: 12/06/2025
+class dowhile
+{
+    public static void main(String[] args) 
+    {
+        int i=1,n=5;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }while(i<=n);
+    }
+}
