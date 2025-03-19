abaca843b614d44c106ae0e1a03102a8a3119af3
GitWhisperer <harshittinker1979@gmail.com>
2025-03-19 15:25:16 +0530

    added files again second time

---
+++ foreach.java
@@ -0,0 +1,8 @@
+class Foreach{
+    public static void main(String[] args){
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (char item : vowels) {
+            System.out.println(item);
+        }
+    }
+}
